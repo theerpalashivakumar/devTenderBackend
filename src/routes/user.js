1c15ee43d9ce61c21d8087aef4289947cbf5849f
theerpalashivakumar <shivakumartheerpal@gmail.com>
2024-11-12 21:52:34 +0530

    update the feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const { userAuth } = require("../middlewares/Auth")
 const ConnectionRequest = require("../modles/connectionRequest")
+const user = require("../modles/user")
+// const connectionRequest = require("../modles/connectionRequest")
 const router = express.Router()
 
 
@@ -22,7 +24,7 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
 
 router.get("/user/connections", userAuth, async (req, res) => {
   try {
-    logginUser = req.user
+    const logginUser = req.user
 
     const connectionRequest = await ConnectionRequest.find({
       $or: [
@@ -48,6 +50,34 @@ router.get("/user/connections", userAuth, async (req, res) => {
   } catch (err) {
     res.status(400).json({ message: err.message })
   }
+})
+
+router.get("/feed",userAuth,async(req,res)=>{
+try{
+    const loggedInUser = req.user;
+   //find all all connections 
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId")
+    //create set for unique 
+    const hideUserFromFeed = new Set()
+    connectionRequests.forEach((eachReq)=>{
+        hideUserFromFeed.add(eachReq.fromUserId);
+        hideUserFromFeed.add(eachReq.toUserId)
+    })
+  //nin=notin ,ne=>notequal feed api dont see itself profile and previous connections these is and quary
+    const users = await user.find({
+        //convert the set to array
+      $and: [{ _id: { $nin: Array.from(hideUserFromFeed) } },{_id:{$ne:loggedInUser._id}}],
+    }).select(user_safe_data)
+   //send all users expect his profile and connections
+    res.send(users)
+
+
+}
+catch(err){
+ res.status(400).json({ message: err.message })
+}
 })
 
 module.exports = router
