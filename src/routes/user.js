c7e70c140443122a84dc435b42277968ea1001f3
theerpalashivakumar <shivakumartheerpal@gmail.com>
2024-11-12 22:39:39 +0530

    pagination based on the query paramaters skip and limit methods in mogoDB

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,6 @@ const user = require("../modles/user")
 // const connectionRequest = require("../modles/connectionRequest")
 const router = express.Router()
 
-
 const user_safe_data = "firstName lastName skills gender"
 //checking the pending request from the users
 router.get("/user/requests/received", userAuth, async (req, res) => {
@@ -36,11 +35,11 @@ router.get("/user/connections", userAuth, async (req, res) => {
       .populate("toUserId", user_safe_data)
 
     const data = connectionRequest.map((each) => {
-      if(each.fromUserId._id.toString() ===logginUser._id.toString()){
+      if (each.fromUserId._id.toString() === logginUser._id.toString()) {
         return each.toUserId
       }
-      
-       return  each.fromUserId
+
+      return each.fromUserId
     })
 
     res.json({
@@ -52,32 +51,41 @@ router.get("/user/connections", userAuth, async (req, res) => {
   }
 })
 
-router.get("/feed",userAuth,async(req,res)=>{
-try{
-    const loggedInUser = req.user;
-   //find all all connections 
+router.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user
+    const page = parseInt(req.query.page) || 1
+    let limit = parseInt(req.query.limit) || 10
+    limit = limit > 50 ? 50 : limit
+    const skip = (page - 1) * limit
+
+    //find all all connections
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId toUserId")
-    //create set for unique 
+    //create set for unique
     const hideUserFromFeed = new Set()
-    connectionRequests.forEach((eachReq)=>{
-        hideUserFromFeed.add(eachReq.fromUserId);
-        hideUserFromFeed.add(eachReq.toUserId)
+    connectionRequests.forEach((eachReq) => {
+      hideUserFromFeed.add(eachReq.fromUserId)
+      hideUserFromFeed.add(eachReq.toUserId)
     })
-  //nin=notin ,ne=>notequal feed api dont see itself profile and previous connections these is and quary
-    const users = await user.find({
+    //nin=notin ,ne=>notequal feed api dont see itself profile and previous connections these is and quary
+    const users = await user
+      .find({
         //convert the set to array
-      $and: [{ _id: { $nin: Array.from(hideUserFromFeed) } },{_id:{$ne:loggedInUser._id}}],
-    }).select(user_safe_data)
-   //send all users expect his profile and connections
+        $and: [
+          { _id: { $nin: Array.from(hideUserFromFeed) } },
+          { _id: { $ne: loggedInUser._id } },
+        ],
+      })
+      .select(user_safe_data)
+      .limit(limit)
+      .skip(skip)
+    //send all users expect his profile and connections
     res.send(users)
-
-
-}
-catch(err){
- res.status(400).json({ message: err.message })
-}
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
 module.exports = router
